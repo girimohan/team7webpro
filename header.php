c547261709bf75227b922d5243861a1cd50b7154
girimohan <45201945+girimohan@users.noreply.github.com>
2022-02-04 23:21:45 +0200

    some changes practise

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
           <ul class="nav nav-tabs">
               <li class="nav-item">
-              <a class="nav-link active" href="index.php">Home</a>
+              <a class="nav-link " href="index.php">Home</a>
               </li>
               <li class="nav-item">
               <a class="nav-link" href="about.php">About Us</a>
