271a58b25a32f8e1ee0077fea5a8140f8cd8a832
girimohan <45201945+girimohan@users.noreply.github.com>
2022-02-16 12:38:18 +0200

    db connection

--- join.php
+++ join.php
@@ -3,26 +3,49 @@
 include 'header.php';
 ?>
 <main>
-            <p>
+            <!-- <p>
                 <h3>
                     Membership Plan
                 </h3>
                 <img src="html-website/images/pic7.jpg" class="img-fluid w-100" alt="">
-            </p>
-            <div>
-              <div class="prices">
-                <h3>Check the Best Package and Prices!</h3>
-                <ol>
-                  <li><a href="#">Basic Prices</a></li>
-                  <li><a href="#">Premium Prices</a></li>
-                  <li><a href="#">Membership Prices</a></li>
-                  <li><a href="#">Student Discount Prices</a></li>
-                  <li><a href="#">OG Yearly Package Prices</a></li>
-                </ol>
-              </div>
-            </div>
+            </p> -->
+            
+<!-- starting a div for signup -->
+            
+  <div class="">
+  <div id="box">
+		<h4>Enroll</h4>
+		<form method="post">
 
-        </main>
+			<input id="text" type="text" name="user_name"placeholder="User Name"><br><br>
+			<input id="text" type="password" name="password" placeholder="Password"><br><br>
+			<input id="text" type="text" name="name" placeholder="Full Name"><br><br>
+			<input id="text" type="email" name="email" placeholder="Email" ><br><br>
+			<input id="text" type="text" name="address" placeholder="Address"><br><br>
+
+			<input id="button" type="submit" value="Signup"><b><br><br>
+
+			<a href="login.php">Click to Login</a><br><br>
+		</form>
+	</div>
+  </div>
+            
+            
+<!--end of sign up form  -->
+  <div>
+    <div class="prices">
+      <h3>Check the Best Package and Prices!</h3>
+      <ol>
+        <li><a href="#">Basic Prices</a></li>
+        <li><a href="#">Premium Prices</a></li>
+        <li><a href="#">Membership Prices</a></li>
+        <li><a href="#">Student Discount Prices</a></li>
+        <li><a href="#">OG Yearly Package Prices</a></li>
+      </ol>
+    </div>
+  </div>
+
+</main>
 <?php
 include 'footer.php';
 ?>
