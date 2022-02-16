f2d408f668708ceb6978450db849252a11854ae4
JoniEnska <joni.hyovalti@gmail.com>
2022-02-16 12:13:41 +0200

    Update about.php

--- about.php
+++ about.php
@@ -2,6 +2,86 @@
 include 'header.php';
 ?>
 
+    <main>
+                <div class="row">
+                  <div class="col-md-6">
+                    <h2>
+                      Who we are!
+                  </h2>
+                  <p>
+                    We olor sit amet, consectetur adipisicing elit. Totam tempora esse tempore dolore dolor
+                     ducimus sed eius corporis doloribus iusto explicabo nostrum a eligendi aspernatur ullam officiis,
+                      modi rem nisi minima, iste, velit necessitatibus? Perferendis quo fugiat aliquam quos repudiandae 
+                      earum accusamus! Maiores harum, eius eligendi saepe labore fugiat libero! Commodi perferendis 
+                      cumque deserunt maxime voluptas hic eos, voluptatibus fugit, molestiae sunt amet qui. Iusto veniam
+                       exercitationem veritatis ut, odio nam dicta. Vitae fugit nam libero veniam rem rerum eveniet atque
+                        tenetur quam dolorem, 
+                    sit voluptas consequuntur maiores maxime blanditiis odio ipsum, deserunt dolor velit inventore. 
+                    Maxime laboriosam volupta.
+                  </p>
+                  </div>
+
+                    <div class="col-md-6">
+                      <h2>
+                        <br>
+                    </h2>
+                    <img src="images/logo_wall.png" class="img-fluid w-100"alt="">
+
+                    </div>
+                  </div>
+                <div class="row">
+                    <div class="col-md-6">
+                      <h2>
+                        What we do!
+                    </h2>
+                    <p>
+                      For dolor sit amet, consectetur adipisicing elit. Totam tempora esse tempore dolore dolor
+                      ducimus sed eius corporis doloribus iusto explicabo nostrum a eligendi aspernatur ullam officiis,
+                        modi rem nisi minima, iste, velit necessitatibus? Perferendis quo fugiat aliquam quos repudiandae 
+                        earum accusamus! Maiores harum, eius eligendi saepe labore fugiat libero! Commodi perferendis 
+                        cumque deserunt maxime voluptas hic eos, voluptatibus fugit, molestiae sunt amet qui. Iusto veniam
+                        exercitationem veritatis ut, odio nam dicta. Vitae fugit nam libero veniam rem rerum eveniet atque
+                          tenetur quam dolorem, 
+                      sit voluptas consequuntur maiores maxime blanditiis odio ipsum, deserunt dolor velit inventore. 
+                      Maxime laboriosam voluptas us.
+                    </p>
+                    </div>
+
+                      <div class="col-md-6">
+                        <h2>
+                          Member of:
+                      </h2>
+                      <img src="images/new-logo2.png" class="img-fluid w-100"alt="">
+
+                      </div>
+                  </div><hr>
+
+                  <div class="column text-center">
+                    <h3>Our Personal Trainer</h3>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-4">
+                      <a href="#"><img src="images/mohan.jpg"  class="blog__thumb-img">
+                        <p class="blog__thumb-title">Mohan Giri</p>
+                      </a>
+                    </div>
+                    <div class="col-md-4">
+                      <a href="#"><img src="images/joni.jpg"  class="blog__thumb-img">
+                          <p class="blog__thumb-title">Joni Hyövälti</p>
+                      </a>
+                    </div>
+                    <div class="col-md-4">
+                      <a href="#"><img src="images/edgar.jpg"  class="blog__thumb-img">
+                          <p class="blog__thumb-title">Edgar Luks</p>
+                      </a>
+                    </div>
+                  </div><hr>
+                
+    </main>
+
+        <article>
+        </article>
+
 <?php
 include 'footer.php';
 ?>
