8abe331cd9753d2795695c159be68dd0366ec47a
JoniEnska <joni.hyovalti@gmail.com>
2022-02-16 13:01:23 +0200

    Update AdminJavaTest.php

--- AdminJavaTest.php
+++ AdminJavaTest.php
@@ -20,41 +20,44 @@ include 'header.php';
         <p id="GFG_DOWN"></p>
         <script>
             var down = document.getElementById("GFG_DOWN");
+            var count = 0;
             function GFG_Fun() {
-               
+                count++;
+                if(count == 1) {
+                    var form = document.createElement("form");
+                    form.setAttribute("method", "post");
+                    form.setAttribute("action", "submit.php");
+    
+                    
+                    var ID = document.createElement("input");
+                    ID.setAttribute("type", "text");
+                    ID.setAttribute("name", "emailID");
+                    ID.setAttribute("placeholder", "E-Mail ID");
+    
+                    
+                    var PWD = document.createElement("input");
+                    PWD.setAttribute("type", "password");
+                    PWD.setAttribute("name", "password");
+                    PWD.setAttribute("placeholder", "Password");
+    
+                    
+                    var s = document.createElement("input");
+                    s.setAttribute("type", "submit");
+                    s.setAttribute("value", "Submit");
+    
+                    
+                    form.append(ID);
                 
-                var form = document.createElement("form");
-                form.setAttribute("method", "post");
-                form.setAttribute("action", "submit.php");
- 
+                    
+                    form.append(PWD);
                 
-                var ID = document.createElement("input");
-                ID.setAttribute("type", "text");
-                ID.setAttribute("name", "emailID");
-                ID.setAttribute("placeholder", "E-Mail ID");
- 
+                    
+                    form.append(s);
+    
+                    document.getElementsByTagName("body")[0]
+                .appendChild(form);
+                }
                 
-                var PWD = document.createElement("input");
-                PWD.setAttribute("type", "password");
-                PWD.setAttribute("name", "password");
-                PWD.setAttribute("placeholder", "Password");
- 
-                
-                var s = document.createElement("input");
-                s.setAttribute("type", "submit");
-                s.setAttribute("value", "Submit");
- 
-                
-                form.append(ID);
-               
-                
-                form.append(PWD);
-               
-                
-                form.append(s);
- 
-                document.getElementsByTagName("body")[0]
-               .appendChild(form);
             }
         </script>
     </body>
