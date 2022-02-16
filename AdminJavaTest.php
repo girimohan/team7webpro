461f943b63668bc4596a25f2512493b8bc83053f
JoniEnska <joni.hyovalti@gmail.com>
2022-02-16 12:37:44 +0200

    Update AdminJavaTest.php

--- AdminJavaTest.php
+++ AdminJavaTest.php
@@ -8,7 +8,7 @@ include 'header.php';
         </title>
     </head>
     <body style="text-align: center;">
-        <h1 style="color: green;">
+        <h1 style="color: black;">
             Admin Login
         </h1>
         <p>
@@ -22,35 +22,35 @@ include 'header.php';
             var down = document.getElementById("GFG_DOWN");
             function GFG_Fun() {
                
-                // Create a form dynamically
+                
                 var form = document.createElement("form");
                 form.setAttribute("method", "post");
                 form.setAttribute("action", "submit.php");
  
-                // Create an input element for emailID
+                
                 var ID = document.createElement("input");
                 ID.setAttribute("type", "text");
                 ID.setAttribute("name", "emailID");
                 ID.setAttribute("placeholder", "E-Mail ID");
  
-                // Create an input element for password
+                
                 var PWD = document.createElement("input");
                 PWD.setAttribute("type", "password");
                 PWD.setAttribute("name", "password");
                 PWD.setAttribute("placeholder", "Password");
  
-                // Create a submit button
+                
                 var s = document.createElement("input");
                 s.setAttribute("type", "submit");
                 s.setAttribute("value", "Submit");
  
-                // Append the email_ID input to the form
+                
                 form.append(ID);
                
-                // Append the password to the form
+                
                 form.append(PWD);
                
-                // Append the button to the form
+                
                 form.append(s);
  
                 document.getElementsByTagName("body")[0]
